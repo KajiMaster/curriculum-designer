b6afadbe2693c377e2916ceff496290d0fdb3893
Blake McNeal <blake.mcneal@gmail.com>
2025-08-20 17:00:14 -0300

    deploy: test complete pipeline with IAM permissions fixed

--- terraform/environments/global/main.tf
+++ terraform/environments/global/main.tf
@@ -149,6 +149,7 @@ resource "aws_iam_role_policy" "github_actions_lambda_deploy" {
           "lambda:UpdateFunctionCode",
           "lambda:UpdateFunctionConfiguration",
           "lambda:GetFunction",
+          "lambda:GetFunctionConfiguration",
           "lambda:InvokeFunction"
         ]
         Resource = [
